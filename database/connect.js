5145475dbd960f851555f3a3149e16d625dd4a03
Saber-Bjeoui <bjeoui.saber@gmail.com>
2020-10-06 22:24:53 +0100

    adding companies route for add and read

--- database/connect.js
+++ database/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const db = mongoose.connect("mongodb://localhost/whwh", {
+const db = mongoose.connect("mongodb://localhost/legacy_project", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
