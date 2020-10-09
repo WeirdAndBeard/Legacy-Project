99355d286fbcbc4e3ed4069e3ddd700e0f548ac7
Yassine-Knaizia <yassine.knaizia@esprit.tn>
2020-10-09 21:31:50 +0100

    done

--- server/routes/chatMessages.router.js
+++ server/routes/chatMessages.router.js
@@ -1,10 +1,10 @@
 const chatRouter = require("express").Router();
-const Chat = require("../models/chatMessages.js");
+const ChatMessage = require("../models/chatMessages.js");
 
 // Get Chat messages
 chatRouter.get("/chat", async (req, res) => {
     try {
-        const chat = await Chat.find();
+        const chat = await ChatMessage.find();
         res.send(chat);
     } catch (error) {
         console.log(error);
@@ -15,7 +15,7 @@ chatRouter.get("/chat", async (req, res) => {
 // Add new message
 chatRouter.post("/chat/add", async (req, res) => {
     try {
-        const chat = new Chat(req.body);
+        const chat = new ChatMessage(req.body);
         const result = await chat.save();
         console.log(req.body);
         res.send(result);
@@ -26,14 +26,14 @@ chatRouter.post("/chat/add", async (req, res) => {
 
 // Delete Message
 chatRouter.delete("/delete/:id", (req, res) => {
-    Chat.deleteOne({ _id: req.params.id }, (err, data) => {
+    ChatMessage.deleteOne({ _id: req.params.id }, (err, data) => {
         err ? console.log(err) : res.send('success');
     })
 })
 
 // Edit Message
 chatRouter.post("/update/:id", (req, res) => {
-    Chat.updateOne({ _id: req.params.id }, { $set: req.body }, (err, data) => {
+    ChatMessage.updateOne({ _id: req.params.id }, { $set: req.body }, (err, data) => {
         err ? console.log(err) : res.send(data);
     })
 })
