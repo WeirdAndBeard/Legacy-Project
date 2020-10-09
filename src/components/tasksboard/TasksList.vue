313bc11e7ed2ed459a4003073514eb25544d8c1e
Saber-Bjeoui <bjeoui.saber@gmail.com>
2020-10-09 21:16:14 +0100

    drag and drop works on fronten

--- src/components/tasksboard/TasksList.vue
+++ src/components/tasksboard/TasksList.vue
@@ -26,14 +26,23 @@
             </button>
           </form>
         </div>
-        <Task v-for="task in tasks" :key="task._id" :task="task" />
+        <draggable
+          v-model="tasks"
+          group="people"
+          @start="drag = true"
+          @end="drag = false"
+        >
+          <transition-group>
+            <Task v-for="task in tasks" :key="task._id" :task="task" />
+          </transition-group>
+        </draggable>
       </b-list-group>
     </b-card>
   </b-card-group>
 </template>
 <script>
 import Task from "./Task.vue";
-
+import draggable from "vuedraggable";
 import axios from "axios";
 
 export default {
@@ -49,6 +58,7 @@ export default {
   },
   components: {
     Task,
+    draggable,
   },
   mounted: function() {
     this.getTasks();
