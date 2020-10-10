7701e176144a04f0ee95b104490c9dfb4d6664b6
Yassine-Knaizia <yassine.knaizia@esprit.tn>
2020-10-10 01:41:34 +0100

    linked

--- server/routes/chatMessages.router.js
+++ server/routes/chatMessages.router.js
@@ -2,41 +2,44 @@ const chatRouter = require("express").Router();
 const ChatMessage = require("../models/chatMessages.js");
 
 // Get Chat messages
-chatRouter.get("/chat", async (req, res) => {
-    try {
-        const chat = await ChatMessage.find();
-        res.send(chat);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error);
-    }
+chatRouter.get("/", async (req, res) => {
+  try {
+    const chat = await ChatMessage.find();
+    res.send(chat);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 });
 
 // Add new message
-chatRouter.post("/chat/add", async (req, res) => {
-    try {
-        const chat = new ChatMessage(req.body);
-        const result = await chat.save();
-        console.log(req.body);
-        res.send(result);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+chatRouter.post("/add", async (req, res) => {
+  try {
+    const chat = new ChatMessage(req.body);
+    const result = await chat.save();
+    console.log(req.body);
+    res.send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 // Delete Message
 chatRouter.delete("/delete/:id", (req, res) => {
-    ChatMessage.deleteOne({ _id: req.params.id }, (err, data) => {
-        err ? console.log(err) : res.send('success');
-    })
-})
+  ChatMessage.deleteOne({ _id: req.params.id }, (err, data) => {
+    err ? console.log(err) : res.send("success");
+  });
+});
 
 // Edit Message
 chatRouter.post("/update/:id", (req, res) => {
-    ChatMessage.updateOne({ _id: req.params.id }, { $set: req.body }, (err, data) => {
-        err ? console.log(err) : res.send(data);
-    })
-})
-
+  ChatMessage.updateOne(
+    { _id: req.params.id },
+    { $set: req.body },
+    (err, data) => {
+      err ? console.log(err) : res.send(data);
+    }
+  );
+});
 
 module.exports = chatRouter;
--- server/routes/companies.router.js
+++ server/routes/companies.router.js
@@ -1,8 +1,10 @@
 const companyRouter = require("express").Router();
 const Company = require("../models/companies.js");
 
-// Get unique company for each employee
-companyRouter.get("/companies", async (req, res) => {
+/**
+ * Get all companies of a user
+ */
+companyRouter.get("/", async (req, res) => {
   try {
     const companies = await Company.find();
     res.send(companies);
@@ -11,9 +13,23 @@ companyRouter.get("/companies", async (req, res) => {
     res.status(500).json(error);
   }
 });
+/**
+ * Get one company by id
+ */
+companyRouter.get("/:id", async (req, res) => {
+  try {
+    const company = await Company.findOne({ _id: req.params.id });
+    res.send(company);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
 
-// Create new Company
-companyRouter.post("api/companies/add", async (req, res) => {
+/**
+ * Create new company
+ */
+companyRouter.post("/add", async (req, res) => {
   try {
     const company = new Company(req.body);
     const result = await company.save();
@@ -26,19 +42,36 @@ companyRouter.post("api/companies/add", async (req, res) => {
       .send("Error: error happend when trying to create new company");
   }
 });
+/**
+ * Delete company
+ */
+companyRouter.delete("/delete/:id", (req, res) => {
+  Company.deleteOne({ _id: req.params.id }, (err, data) => {
+    err ? console.log({ err }) : res.send("success");
+  });
+});
+
+/**
+ * Update company
+ */
+companyRouter.post("/update/:id", (req, res) => {
+  Company.updateOne({ _id: req.params.id }, req.body, (err, data) => {
+    err ? console.log(err) : res.send(data);
+  });
+});
 
 // Delete Company
 companyRouter.delete("/delete/:id", (req, res) => {
   Company.deleteOne({ _id: req.params.id }, (err, data) => {
-      err ? console.log(err) : res.send('success');
-  })
-})
+    err ? console.log(err) : res.send("success");
+  });
+});
 
 // Edit Company
 companyRouter.post("/update/:id", (req, res) => {
-  Company.updateOne({ _id: req.params.id }, {$set:req.body}, (err, data) => {
-      err ? console.log(err) : res.send(data);
-  })
-})
+  Company.updateOne({ _id: req.params.id }, { $set: req.body }, (err, data) => {
+    err ? console.log(err) : res.send(data);
+  });
+});
 
 module.exports = companyRouter;
